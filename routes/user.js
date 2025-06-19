ec3e5a1364ba2ee18b8a5ad5b2ea97d04ca60643
klao258 <klao25388@gmail.com>
2025-06-20 02:44:48 +0800

    优化

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const router = new Router({ prefix: '/user' });
 
 router.post('/sync', userController.syncUser);  // 同步用户数据
 router.post('/batch', userController.getUserBatch);  // 根据条件获取用户列表
-router.get('/getAccoutPost', userController.getUsers);   // 获取账号帖子汇总数据
+router.get('/getAccoutPost', userController.getAccoutPost);   // 获取账号帖子汇总数据
 
 
 module.exports = router;
