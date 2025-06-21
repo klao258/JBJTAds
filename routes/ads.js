e03dc56a78db724494abda60ad3a953f9b2298ec
klao258 <klao25388@gmail.com>
2025-06-21 17:25:12 +0800

    优化命名

--- routes/ads.js
+++ routes/ads.js
@@ -3,7 +3,7 @@ const adsController = require('../controllers/adsController');
 
 const router = new Router({ prefix: '/ads' });
 
-router.post('/sync', adsController.syncAds);    // 同步广告信息
+router.post('/syncAds', adsController.syncAds);    // 同步广告信息
 router.post('/recordCpm', adsController.recordCpmChange);   // 记录CPM变更
 router.get('/cpmList', adsController.getCpmChanges);    // 获取CPM变更列表
 router.post('/recordView', adsController.recordDailyView);  //  记录日常浏览量
