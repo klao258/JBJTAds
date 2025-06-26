7d239e2b53104594e522507c507a0fd45dc2c333
klao258 <klao25388@gmail.com>
2025-06-26 17:14:27 +0800

    优化

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,10 @@
 // ecosystem.config.js
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default {
     apps: [
       {
