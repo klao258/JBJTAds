bdf4e80276bf34c83f9f11e5b3c800439058f9ff
klao258 <klao25388@gmail.com>
2025-06-28 00:49:41 +0800

    取消下标

--- controllers/userController.js
+++ controllers/userController.js
@@ -331,9 +331,8 @@ export const getTodayStats = async ctx => {
   }
 
   // 转换结果为数组结构
-  const userStats = Object.values(platformMap).map((plat, index) => {
+  const userStats = Object.values(platformMap).map(plat => {
     return {
-      no: index,
       platform: plat.platform,
       regCount: plat.regCount,
       payCount: plat.payCount,
@@ -344,11 +343,10 @@ export const getTodayStats = async ctx => {
 
   // ✅ 广告账号角度统计（ads 拆解后取下标为1）
   const accountStats = {};
-  for (const [index, user] of users.entries()) {
+  for (const user of users) {
     const adsAccount = user.ads?.split('-')?.[1] || '未知';
     if (!accountStats[adsAccount]) {
       accountStats[adsAccount] = {
-        no: index,
         adsAccount,
         regCount: 0,
         payCount: 0,
@@ -372,11 +370,10 @@ export const getTodayStats = async ctx => {
   }
 
   const postStats = {};
-  for (const [index, user] of users.entries()) {
+  for (const user of users) {
     const title = titleMap[user.ads] || '未知';
     if (!postStats[title]) {
       postStats[title] = {
-        no: index,
         title,
         regCount: 0,
         payCount: 0,
