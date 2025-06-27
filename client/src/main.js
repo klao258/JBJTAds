f2cffdafbddd25c4b829abe6489a090cc70a7738
klao258 <klao25388@gmail.com>
2025-06-28 01:51:25 +0800

    设置中文面板

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { create, zhCN } from 'naive-ui'
+
+const naive = create({
+  locale: zhCN
+})
 
 const app = createApp(App)
+app.use(naive)
 app.use(router)
 app.mount('#app')
