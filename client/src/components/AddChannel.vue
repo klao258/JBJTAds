3a43514efc68c7ee5eedbbdbee5a19217616a8c9
klao258 <klao25388@gmail.com>
2025-07-16 17:23:49 +0800

    代码优化

--- client/src/components/AddChannel.vue
+++ client/src/components/AddChannel.vue
@@ -1,164 +1,249 @@
 <template>
-    <div>
-        <n-upload :on-change="onFileChange" accept=".xlsx, .xls">
-            <n-button type="primary">添加频道</n-button>
-        </n-upload>
-  
-        <n-modal v-model:show="showModal" title="表格数据" :auto-focus="false" @close="resetData">
-            <n-card style="width: 98%;">
-                <n-data-table v-if="tableData.length"
-                    :columns="columns"
-                    :data="tableData"
-                    :max-height="400"
-                    :row-key="(row) => row.url" />
-                <p v-else>没有数据显示</p>
-                <template #action>
-                    <div class="flex flex-between flex-center-y">
-                        <div>共{{ tableData.length - 1 }}条数据</div>
-                        <div class="flex flex-end">
-                            <n-button type="primary" @click="showModal = false">开始添加</n-button>
-                            <n-button @click="showModal = false">关闭</n-button>
-                        </div>
-                    </div>
-                </template>
-            </n-card>
-        </n-modal>
-    </div>
-</template>
-  
-<script setup>
-import { getChannelType } from '@/api';
-import * as XLSX from 'xlsx';
-import { NSelect } from "naive-ui";
-import { onMounted } from 'vue';
+	<div>
+		<n-upload :on-change="onFileChange" accept=".xlsx, .xls">
+			<n-button type="primary">添加频道</n-button>
+		</n-upload>
 
-  
+		<n-modal
+			v-model:show="showModal"
+			title="表格数据"
+			:auto-focus="false"
+			@close="resetData"
+		>
+			<n-card style="width: 98%">
+				<n-data-table
+					v-if="tableData.length"
+					:columns="columns"
+					:data="tableData"
+					:max-height="400"
+					:row-key="(row) => row.url"
+				/>
+				<p v-else>没有数据显示</p>
+				<template #action>
+					<div class="flex flex-between flex-center-y">
+						<div>共{{ tableData.length - 1 }}条数据</div>
+						<div class="flex flex-end">
+							<n-button type="primary" @click="addChannelFn"
+								>开始添加</n-button
+							>
+							<n-button @click="showModal = false">关闭</n-button>
+						</div>
+					</div>
+				</template>
+			</n-card>
+		</n-modal>
+	</div>
+</template>
+
+<script setup>
+import * as XLSX from 'xlsx';
+import { NSelect } from 'naive-ui';
+import { onMounted, ref } from 'vue';
+
+const props = defineProps({
+	channelType: {
+		type: Array,
+		default: () => [],
+	},
+});
+
 const showModal = ref(false);
 const tableData = ref([]);
 const columns = ref([]);
 const columnsObj = {
-    title: { title: '标题', key: 'title', ellipsis: true, width: 150 },
-    url: { title: '链接', key: 'url', ellipsis: true, width: 100, render(row) {
-            return row.url?.replace(/https\:\/\/t.me\//, '@')
-        } 
-    },
-    sourceType: { title: 'TG类型', key: 'sourceType', ellipsis: true, width: 80, render(row, index) {
-            return h(NSelect, {
-                value: row.sourceType,
-                style: { width: '100px' },
-                options: [
-                    { label: '机器人', value: '机器人'}, 
-                    { label: '频道', value: '频道' }, 
-                    { label: '群组', value: '群组' }
-                ],
-                onUpdateValue(v) {
-                    tableData.value[index].sourceType = v;
-                }
-            });
-        } 
-    },
-    isAdvertised: { title: '是否推广', key: 'isAdvertised', ellipsis: true, width: 80, render(row, index) {
-            return h(NSelect, {
-                value: row.isAdvertised,
-                style: { width: '80px' },
-                options: [
-                    { label: '是', value: true }, 
-                    { label: '否', value: false }, 
-                ],
-                onUpdateValue(v) {
-                    tableData.value[index].isAdvertised = v;
-                }
-            });
-        } 
-    },
-    typeId: { title: '频道类型', key: 'typeId', ellipsis: true, width: 80 },
-    hasOrders: { title: '是否出单', key: 'hasOrders', ellipsis: true, width: 80, render(row, index) {
-            return h(NSelect, {
-                value: row.hasOrders,
-                style: { width: '80px' },
-                options: [
-                    { label: '是', value: true }, 
-                    { label: '否', value: false }, 
-                ],
-                onUpdateValue(v) {
-                    tableData.value[index].hasOrders = v;
-                }
-            });
-        } 
-    },
-    grade: { title: '评分', key: 'grade', ellipsis: true, width: 80, render(row, index) {
-            return h(NSelect, {
-                value: row.grade,
-                style: { width: '80px' },
-                options: [
-                    { label: 'A+', value: 'A+' }, 
-                    { label: 'A', value: 'A' }, 
-                    { label: 'B+', value: 'B+' },
-                    { label: 'B', value: 'B' }, 
-                    { label: 'C+', value: 'C+' }, 
-                    { label: 'C', value: 'C' },
-                    { label: 'D+', value: 'D+' }, 
-                    { label: 'D', value: 'D' }
-                ],
-                onUpdateValue(v) {
-                    tableData.value[index].grade = v;
-                }
-            });
-        } 
-    },
-    subscribers: { title: '订阅数', key: 'subscribers', ellipsis: true, width: 80 },
-    description: { title: '描述', key: 'description', ellipsis: true, width: 200 }
+	title: { title: '标题', key: 'title', ellipsis: true, width: 150 },
+	url: {
+		title: '链接',
+		key: 'url',
+		ellipsis: true,
+		width: 100,
+		render(row) {
+			return row.url?.replace(/https\:\/\/t.me\//, '@');
+		},
+	},
+	sourceType: {
+		title: 'TG类型',
+		key: 'sourceType',
+		ellipsis: true,
+		width: 100,
+		render(row, index) {
+			return h(NSelect, {
+				value: row.sourceType,
+				style: { width: '100px' },
+				options: [
+					{ label: '机器人', value: '机器人' },
+					{ label: '频道', value: '频道' },
+					{ label: '群组', value: '群组' },
+				],
+				onUpdateValue(v) {
+					tableData.value[index].sourceType = v;
+				},
+			});
+		},
+	},
+	isAdvertised: {
+		title: '是否推广',
+		key: 'isAdvertised',
+		ellipsis: true,
+		width: 80,
+		render(row, index) {
+			return h(NSelect, {
+				value: row.isAdvertised,
+				style: { width: '80px' },
+				options: [
+					{ label: '是', value: 1 },
+					{ label: '否', value: 0 },
+				],
+				onUpdateValue(v) {
+					tableData.value[index].isAdvertised = v;
+				},
+			});
+		},
+	},
+	typeId: {
+		title: '频道类型',
+		key: 'typeId',
+		ellipsis: true,
+		width: 190,
+		render(row, index) {
+			return h(NSelect, {
+				value: row.typeId,
+				style: { width: '190px' },
+				options: props.channelType,
+				onUpdateValue(v) {
+					tableData.value[index].typeId = v;
+				},
+			});
+		},
+	},
+	hasOrders: {
+		title: '是否出单',
+		key: 'hasOrders',
+		ellipsis: true,
+		width: 80,
+		render(row, index) {
+			return h(NSelect, {
+				value: row.hasOrders,
+				style: { width: '80px' },
+				options: [
+					{ label: '是', value: 1 },
+					{ label: '否', value: 0 },
+				],
+				onUpdateValue(v) {
+					tableData.value[index].hasOrders = v;
+				},
+			});
+		},
+	},
+	grade: {
+		title: '评分',
+		key: 'grade',
+		ellipsis: true,
+		width: 80,
+		render(row, index) {
+			return h(NSelect, {
+				value: row.grade,
+				style: { width: '80px' },
+				options: [
+					{ label: 'A+', value: 'A+' },
+					{ label: 'A', value: 'A' },
+					{ label: 'B+', value: 'B+' },
+					{ label: 'B', value: 'B' },
+					{ label: 'C+', value: 'C+' },
+					{ label: 'C', value: 'C' },
+					{ label: 'D+', value: 'D+' },
+					{ label: 'D', value: 'D' },
+				],
+				onUpdateValue(v) {
+					tableData.value[index].grade = v;
+				},
+			});
+		},
+	},
+	subscribers: {
+		title: '订阅数',
+		key: 'subscribers',
+		ellipsis: true,
+		width: 80,
+	},
+	description: {
+		title: '描述',
+		key: 'description',
+		ellipsis: true,
+		width: 150,
+	},
 };
 
-onMounted(async () => {
-    await getChannelTypeFn();
-});
-
-// 获取行业分类
-const getChannelTypeFn = async () => {
-    const res = await getChannelType();
-    console.log('获取频道类型:', res);
-        // columnsObj.typeId.options = res.data.map(item => ({ label: item.name, value: item.id }));
-};
-  
 const onFileChange = ({ file }) => {
-    // 确保获取到文件对象
-    if (file && file.file instanceof Blob) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const data = new Uint8Array(e.target.result);
-            const workbook = XLSX.read(data, { type: 'array' });
-            const firstSheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[firstSheetName];
+	// 确保获取到文件对象
+	if (file && file.file instanceof Blob) {
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			const data = new Uint8Array(e.target.result);
+			const workbook = XLSX.read(data, { type: 'array' });
+			const firstSheetName = workbook.SheetNames[0];
+			const worksheet = workbook.Sheets[firstSheetName];
 
-            const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-            columns.value = jsonData[0].map(item => {
-                const column = columnsObj[item] || { title: item, key: item, ellipsis: true };
-                return column
-            });
-            tableData.value = jsonData.slice(1).map(row => {
-                return row.reduce((acc, value, index) => {
-                acc[jsonData[0][index]] = value;
-                return acc;
-                }, {});
-            });
+			const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+			columns.value = jsonData[0].map((item) => {
+				const column = columnsObj[item] || {
+					title: item,
+					key: item,
+					ellipsis: true,
+				};
+				return column;
+			});
 
-            console.log('表格数据:',columns.value, tableData.value);
+			tableData.value = jsonData.slice(1).map((row) => {
+				return row.reduce((acc, value, index) => {
+					// 将每行数据的 isAdvertised 字段转换
+					if (
+						['isAdvertised', 'hasOrders'].includes(
+							jsonData[0][index]
+						)
+					) {
+						acc[jsonData[0][index]] =
+							value === true || value === '是' ? 1 : 0;
+					} else if (['typeId'].includes(jsonData[0][index])) {
+						// 将 typeId 字段转换为对应的 ID
+						const type = props.channelType?.find(
+							(item) => item.label === value
+						);
+						acc[jsonData[0][index]] = type ? type.value : '';
+					} else {
+						acc[jsonData[0][index]] = value; // 其他字段保持不变
+					}
 
-            // 自动打开模态框
-            showModal.value = true;
-        };
+					return acc;
+				}, {});
+			});
 
-        // 读取文件为 ArrayBuffer
-        reader.readAsArrayBuffer(file.file);
-    } else {
-        console.error('没有选择有效的文件。');
-    }
+			console.log('表格数据:', tableData.value);
+
+			// 自动打开模态框
+			showModal.value = true;
+		};
+
+		// 读取文件为 ArrayBuffer
+		reader.readAsArrayBuffer(file.file);
+	} else {
+		console.error('没有选择有效的文件。');
+	}
 };
-  
+
 const resetData = () => {
-    tableData.value = [];
-    columns.value = [];
+	tableData.value = [];
+	columns.value = [];
+};
+
+// 开始添加频道
+const addChannelFn = async () => {
+	if (tableData.value.length === 0) return;
+
+	// 这里可以添加逻辑来处理添加频道的操作
+	console.log('开始添加频道:', tableData.value);
+
+	// 关闭模态框
+	showModal.value = false;
 };
 </script>
-  
--- client/src/views/Channel.vue
+++ client/src/views/Channel.vue
@@ -1,122 +1,159 @@
 <template>
-    <div class="filter-bar" style="display:flex; gap:16px; margin-bottom:16px;">
-      <n-input
-        v-model:value="filters.title"
-        placeholder="频道标题模糊搜索"
-        clearable
-        style="width: 250px;"
-      />
-      <n-select
-        v-model:value="filters.sourceType"
-        :options="sourceTypeOptions"
-        placeholder="选择来源类型"
-        clearable
-        style="width: 150px;"
-      />
-      <!-- <n-checkbox v-model:value="filters.isAdvertised" label="只显示已推广" /> -->
+	<div
+		class="filter-bar"
+		style="display: flex; gap: 16px; margin-bottom: 16px"
+	>
+		<n-input
+			v-model:value="filters.title"
+			placeholder="频道标题模糊搜索"
+			clearable
+			style="width: 250px"
+		/>
+		<n-select
+			v-model:value="filters.sourceType"
+			:options="sourceTypeOptions"
+			placeholder="选择来源类型"
+			clearable
+			style="width: 150px"
+		/>
+		<!-- <n-checkbox v-model:value="filters.isAdvertised" label="只显示已推广" /> -->
 
-      <n-button @click="getChannelListFn" type="primary">搜索</n-button>
-      <n-button @click="resetFilters">重置</n-button>
-      <AddChannel />
-    </div>
+		<n-button @click="getChannelListFn" type="primary">搜索</n-button>
+		<n-button @click="resetFilters">重置</n-button>
+		<AddChannel :channelType="channelType" />
+	</div>
 
-    <n-data-table
-      :columns="columns"
-      :data="channels"
-      :pagination="{ page: pagination.page, pageSize: pagination.pageSize, itemCount: total }"
-      @update:page="page => { pagination.page = page; getChannelListFn() }"
-      @update:pageSize="pageSize => { pagination.pageSize = pageSize; getChannelListFn() }"
-      :loading="loading"
-      :row-key="(row) => row.shortId" />
-  </template>
-  
-  <script setup>
-  import AddChannel from '@/components/AddChannel.vue';
-  import { getChannelList } from '@/api'
+	<n-data-table
+		:columns="columns"
+		:data="channels"
+		:pagination="{
+			page: pagination.page,
+			pageSize: pagination.pageSize,
+			itemCount: total,
+		}"
+		@update:page="
+			(page) => {
+				pagination.page = page;
+				getChannelListFn();
+			}
+		"
+		@update:pageSize="
+			(pageSize) => {
+				pagination.pageSize = pageSize;
+				getChannelListFn();
+			}
+		"
+		:loading="loading"
+		:row-key="(row) => row.shortId"
+	/>
+</template>
+
+<script setup>
+import AddChannel from '@/components/AddChannel.vue';
+import { getChannelType, getChannelList } from '@/api';
 
 const showModal = ref(false);
-  const filters = reactive({
-    title: '',
-    sourceType: 'bot',
-    isAdvertised: false
-  })
-  
-  const sourceTypeOptions = [
-    { label: '机器人(bot)', value: 'bot' },
-    { label: '频道(channel)', value: 'channel' },
-    { label: '群组(group)', value: 'group' }
-  ]
-  
-  const columns = [
-    { title: '频道ID', key: 'shortId', ellipsis: true },
-    { title: 'TG类型', key: 'sourceType', render(row) {
-        const map = { bot: '机器人', channel: '频道', group: '群组' }
-        return map[row.sourceType] || row.sourceType
-      }
-    },
-    { title: '频道类型', key: 'typeId', ellipsis: true },
-    { title: '标题', key: 'title', ellipsis: true },
-    { title: '描述', key: 'description', ellipsis: true },
-    { title: '订阅数', key: 'subscribers' },
-    { title: '链接', key: 'url' },
-    { title: '是否推广', key: 'isAdvertised', render(row) {
-        return row.isAdvertised ? '是' : '否'
-      }
-    },
-    { title: '是否出单', key: 'hasOrders', render(row) {
-        return row.isAdvertised ? '是' : '否'
-      }
-    },
-    { title: '收录时间', key: 'createdAt', render(row) {
-        return new Date(row.createdAt).toLocaleString()
-      }
-    }
-  ]
-  
-  const channels = ref([])
-  const total = ref(0)
-  const loading = ref(false)
-  const pagination = reactive({
-    page: 1,
-    pageSize: 10
-  })
+const filters = reactive({
+	title: '',
+	sourceType: 'bot',
+	isAdvertised: false,
+});
 
-  onMounted(() => {
-    getChannelListFn()
-  })
-  
-  const getChannelListFn = async () => {
-    loading.value = true
-    try {
-      // 构造查询参数
-      const params = {
-        page: pagination.page,
-        pageSize: pagination.pageSize
-      }
-      if (filters.title) params.title = filters.title
-      if (filters.sourceType) params.sourceType = filters.sourceType
-      if (filters.isAdvertised) params.isAdvertised = true
-  
-      const res = await getChannelList(params)
-      channels.value = res.channels
-      total.value = res.total
-    } catch (error) {
-      console.error('获取频道列表失败', error)
-    } finally {
-      loading.value = false
-    }
-  }
-  
-  const resetFilters = () => {
-    filters.title = ''
-    filters.sourceType = ''
-    filters.isAdvertised = false
-    pagination.page = 1
-    getChannelListFn()
-  }
+const sourceTypeOptions = [
+	{ label: '机器人(bot)', value: 'bot' },
+	{ label: '频道(channel)', value: 'channel' },
+	{ label: '群组(group)', value: 'group' },
+];
 
-  const addChannel = () => {
+const columns = [
+	{ title: '频道ID', key: 'shortId', ellipsis: true },
+	{
+		title: 'TG类型',
+		key: 'sourceType',
+		render(row) {
+			const map = { bot: '机器人', channel: '频道', group: '群组' };
+			return map[row.sourceType] || row.sourceType;
+		},
+	},
+	{ title: '频道类型', key: 'typeId', ellipsis: true },
+	{ title: '标题', key: 'title', ellipsis: true },
+	{ title: '描述', key: 'description', ellipsis: true },
+	{ title: '订阅数', key: 'subscribers' },
+	{ title: '链接', key: 'url' },
+	{
+		title: '是否推广',
+		key: 'isAdvertised',
+		render(row) {
+			return row.isAdvertised ? '是' : '否';
+		},
+	},
+	{
+		title: '是否出单',
+		key: 'hasOrders',
+		render(row) {
+			return row.isAdvertised ? '是' : '否';
+		},
+	},
+	{
+		title: '收录时间',
+		key: 'createdAt',
+		render(row) {
+			return new Date(row.createdAt).toLocaleString();
+		},
+	},
+];
 
-  }
-  </script>
-  
+const channelType = ref([]);
+const channels = ref([]);
+const total = ref(0);
+const loading = ref(false);
+const pagination = reactive({
+	page: 1,
+	pageSize: 10,
+});
+
+onMounted(async () => {
+	await getChannelTypeFn();
+	await getChannelListFn();
+});
+
+const getChannelListFn = async () => {
+	loading.value = true;
+	try {
+		// 构造查询参数
+		const params = {
+			page: pagination.page,
+			pageSize: pagination.pageSize,
+		};
+		if (filters.title) params.title = filters.title;
+		if (filters.sourceType) params.sourceType = filters.sourceType;
+		if (filters.isAdvertised) params.isAdvertised = true;
+
+		const res = await getChannelList(params);
+		channels.value = res.channels;
+		total.value = res.total;
+	} catch (error) {
+		console.error('获取频道列表失败', error);
+	} finally {
+		loading.value = false;
+	}
+};
+
+const resetFilters = () => {
+	filters.title = '';
+	filters.sourceType = '';
+	filters.isAdvertised = false;
+	pagination.page = 1;
+	getChannelListFn();
+};
+
+// 获取行业分类
+const getChannelTypeFn = async () => {
+	const res = await getChannelType();
+	channelType.value =
+		res?.channelTypes?.map?.((item) => ({
+			label: item.name,
+			value: item._id,
+		})) || [];
+};
+</script>
