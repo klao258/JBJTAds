d2b121c2b29eeea126425138fa8f2ce9cfd197c4
klao258 <klao25388@gmail.com>
2025-07-21 20:10:15 +0800

    编辑接口都需要token

--- routes/ads.js
+++ routes/ads.js
@@ -1,14 +1,14 @@
-import Router from 'koa-router'
-import * as adsController from '../controllers/adsController.js'
+import Router from 'koa-router';
+import * as adsController from '../controllers/adsController.js';
 
 const router = new Router({ prefix: '/ads' });
 
-router.post('/syncAds', adsController.syncAds);    // 同步广告信息
-router.post('/recordCpm', adsController.recordCpmChange);   // 记录CPM变更
-router.get('/cpmList', adsController.getCpmChanges);    // 获取CPM变更列表
-router.post('/recordViews', adsController.recordDailyViews);  //  记录日常浏览量
-router.get('/getAdsDailyStats', adsController.getAdsDailyStats);    // 获取近3天views
-router.get('/getAdsUEN', adsController.getAdsUEN);    // 获取每个账号协议号占比（按用户昵称不到中文来判断）
-router.get('/getTodayData', adsController.getTodayData);    // 获取今日数据
+router.post('/syncAds', adsController.syncAds); // 同步广告信息
+router.post('/recordCpm', adsController.recordCpmChange); // 记录CPM变更
+router.get('/cpmList', adsController.getCpmChanges); // 获取CPM变更列表
+router.post('/recordViews', adsController.recordDailyViews); //  记录日常浏览量
+router.get('/getAdsDailyStats', adsController.getAdsDailyStats); // 获取近3天views
+router.get('/getAdsUEN', adsController.getAdsUEN); // 获取每个账号协议号占比（按用户昵称不到中文来判断）
+router.get('/getTodayData', adsController.getTodayData); // 获取今日数据
 
-export default router
+export default router;
