eff6a35c86a952f1a608803a504ca34f1c305b56
klao258 <klao25388@gmail.com>
2025-07-22 11:11:09 +0800

    优化

--- routes/ads.js
+++ routes/ads.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import * as adsController from '../controllers/adsController.js';
+import auth from '../middlewares/auth.js';
 
 const router = new Router({ prefix: '/ads' });
 
@@ -9,6 +10,6 @@ router.get('/cpmList', adsController.getCpmChanges); // 获取CPM变更列表
 router.post('/recordViews', adsController.recordDailyViews); //  记录日常浏览量
 router.get('/getAdsDailyStats', adsController.getAdsDailyStats); // 获取近3天views
 router.get('/getAdsUEN', adsController.getAdsUEN); // 获取每个账号协议号占比（按用户昵称不到中文来判断）
-router.get('/getTodayData', adsController.getTodayData); // 获取今日数据
+router.get('/getTodayData', auth, adsController.getTodayData); // 获取今日数据
 
 export default router;
